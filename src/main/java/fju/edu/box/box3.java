19c5a02d627ed66d8084f6fc08bdc84e27e4c554
GM <nicoyazawa0501@gmail.com>
2021-03-30 16:22:26 +0800

    0330 box3 end

--- src/main/java/fju/edu/box/box3.java
+++ src/main/java/fju/edu/box/box3.java
@@ -1,8 +1,15 @@
 package fju.edu.box;
 
-public class box3 {
-    int length ;
-    int width ;
-    int height ;
-    int cost ;
+public class box3 extends box {
+    public box3(){
+        length = 10;
+        width = 10;
+        height = 10;
+
+    } public  String getName(){
+        return "box3";
+    }
+    public  int getPrice(){
+        return 60 ;
+    }
 }
--- src/main/java/fju/edu/box/boxTaster.java
+++ src/main/java/fju/edu/box/boxTaster.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class boxTaster {
     public static void main(String[] args) {
-        System.out.println("Please enter the height:");
         Scanner scan = new Scanner(System.in);
-        int h = scan.nextInt();
-        System.out.println("Please enter the width:");
-        int w = scan.nextInt();
         System.out.println("Please enter the lenght:");
         int l = scan.nextInt();
+        System.out.println("Please enter the width:");
+        int w = scan.nextInt();
+        System.out.println("Please enter the height:");
+        int h = scan.nextInt();
+
 
     }
 }
