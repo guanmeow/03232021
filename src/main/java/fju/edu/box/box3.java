a6b7fdde4b9411b37e92d42beb9b6df881e3d588
GM <nicoyazawa0501@gmail.com>
2021-03-30 15:53:32 +0800

    0330 box start

---
+++ src/main/java/fju/edu/box/box3.java
@@ -0,0 +1,8 @@
+package fju.edu.box;
+
+public class box3 {
+    int length ;
+    int width ;
+    int height ;
+    int cost ;
+}
---
+++ src/main/java/fju/edu/box/box5.java
@@ -0,0 +1,5 @@
+package fju.edu.box;
+
+public class box5 extends box3{
+
+}
