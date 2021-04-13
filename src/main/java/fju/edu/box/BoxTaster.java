807a87dcf804acbe464cc64272846a9b44d27e68
GM <nicoyazawa0501!@gmail.com>
2021-04-13 14:08:05 +0800

    0413 get box5

--- src/main/java/fju/edu/box/BoxTaster.java
+++ src/main/java/fju/edu/box/BoxTaster.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class BoxTaster {
     public static void main(String[] args) {
         Box3 box3 = new Box3();
+        Box5 box5 = new Box5();
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter  length: ");
         int length = scan.nextInt();
@@ -15,6 +16,12 @@ public class BoxTaster {
         System.out.println("Length: " + length + ", width: " + width + ", height: " + height);
         if (box3.validate(length, width, height)) {
             System.out.println(box3.getName());
+            System.out.println(box3.getPrice());
+        }else if(box5.validate(length, width, height)){
+            System.out.println(box5.getName());
+            System.out.println(box5.getPrice());
+        }else{
+            System.out.println("no~");
         }
     }
 }
