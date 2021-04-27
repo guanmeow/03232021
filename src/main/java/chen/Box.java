e90bb2aa446deab71e9a3ae94b3532d36a40bd40
GM <nicoyazawa0501@gmail.com>
2021-04-27 14:50:04 +0800

    0427 box simple

---
+++ src/main/java/chen/Box.java
@@ -0,0 +1,14 @@
+package chen;
+
+public class Box {
+    int length;
+    int width;
+    int heigth;
+    public boolean volidate(int a , int b , int c){
+        if(length>=a &&width>=b && heigth>=c){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
