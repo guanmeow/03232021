aa3dcf4f1b7cc995b78bbf8f4d42c930109703ee
GM <nicoyazawa0501>
2021-05-04 14:44:20 +0800

    0504 List

---
+++ src/main/java/chen/BetterTaster.java
@@ -0,0 +1,17 @@
+package chen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BetterTaster {
+    public static void main(String[] args) {
+        List boxes = new ArrayList();
+        boxes.add(new Box1());
+        boxes.add(new Box2());
+        boxes.add(new Box3());
+        boxes.add(new Box4());
+
+        Box box = (Box) boxes.get(2);
+        System.out.println(box.name);
+    }
+}
