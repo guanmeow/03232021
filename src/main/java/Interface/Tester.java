74f799c5aaf5bbd149f77ed129d107414b974a1e
GM <nicoyazawa0501>
2021-05-11 16:06:49 +0800

    0511 abstract interface 共用一個main且不用另增類別

--- src/main/java/Interface/Tester.java
+++ src/main/java/Interface/Tester.java
@@ -2,10 +2,24 @@ package Interface;
 
 public class Tester {
     public static void main(String[] args) {
-        Reporter r = new Reporter();
-        r.print();
-        Reporter2 r2 = new Reporter2();
-        r2.sayhi();
+//        Reporter r = new Reporter();
+//        r.print();
+//        Reporter2 r2 = new Reporter2();
+//        r2.sayhi();
+        Print p = new Print() {
+            @Override
+            public void print() {
+                System.out.println("say hi");
+            }
+        };
+        Print p1 = new Print() {
+            @Override
+            public void print() {
+                System.out.println("hi");
+            }
+        };
+        p.print();
+        p1.print();
     }
 
 
